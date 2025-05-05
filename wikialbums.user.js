24f9437f1fb380333973cb9c9a7227ff8c2d10be
Josh Braendel <Jbud@live.ca>
2025-05-04 19:47:33 -0700

    test

--- wikialbums.user.js
+++ wikialbums.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         List Of Albums - Player Injection
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://en.wikipedia.org/wiki/*
@@ -24,7 +24,7 @@
 (function() {
     'use strict';
 
-    console.log("TESTING GREASEMONKEY UPDATE: 4");
+    console.log("TESTING GREASEMONKEY UPDATE: 5");
 
     let youtubeApiKey = '';
 
